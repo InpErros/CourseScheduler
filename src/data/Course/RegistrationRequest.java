0d50cb125f12233eab88a0fe500cf8e17fef2de5
Lucas Demchik <lucasd1099@gmail.com>
2020-05-19 13:23:30 -0700

    made verbage more clear

--- src/data/Course/RegistrationRequest.java
+++ src/data/Course/RegistrationRequest.java
@@ -7,20 +7,20 @@ import data.Database.*;
 import java.util.ArrayList;
 
 public class RegistrationRequest {
-    private Student s;
+    private Student student;
     private ArrayList<Course> courses;
     private StudentDatabase sdb = StudentDatabase.getInstance();
     private CourseDatabase cdb = CourseDatabase.getInstance();
 
     public RegistrationRequest(String[] arr) throws NotFoundException{
-        this.s = sdb.findStudent(arr[0]);
+        this.student = sdb.findStudent(arr[0]);
         for(int i = 1; i < arr.length; ++i){
             courses.add(cdb.findCourse(arr[i]));
         }
     }
 
 
-    public Student getS() { return s; }
+    public Student getStudent() { return student; }
 
     public ArrayList<Course> getCourses() { return courses; }
 }
