25a9fc14d45c4c53cd0e618d58af7b9e471be14d
Lucas Demchik <lucasd1099@gmail.com>
2020-05-18 23:51:27 -0700

    Layed out plans for the major function

--- src/app/Algorithms/FirstComeFirstServeScheduler.java
+++ src/app/Algorithms/FirstComeFirstServeScheduler.java
@@ -16,3 +16,14 @@ public class FirstComeFirstServeScheduler implements Scheduler {
         }
     }
 }
+
+
+/**
+ * Schedule(){
+ * Take first Request ->for loop
+ * First Course Does it have a session ->for loop
+ * hasSession() checks for session and creates one if there isn't one. ->assign Faculty -> add to Unscheduled if no faculty available
+ * And/ Or add student to the session
+ * Add Student to the scheduled list
+ *
+ */
