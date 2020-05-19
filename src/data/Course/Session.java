cf854b8b332928857d5aa4b29fd2d2afbfc9e7e7
Lucas Demchik <lucasd1099@gmail.com>
2020-05-18 23:48:20 -0700

    Edited Session to contain a list of students and an instructor

--- src/data/Course/Session.java
+++ src/data/Course/Session.java
@@ -1,10 +1,16 @@
 package data.Course;
 
 import app.Algorithms.IDgenerator;
+import data.Person.Faculty;
+import data.Person.Student;
+
+import java.util.ArrayList;
 
 public class Session extends Course {
     private String id;
     private IDgenerator generatorAlgorithm;
+    private ArrayList<Student> students;
+    private Faculty instructor;
 
     public Session(String department, String code, String description, int minStudentCount, int maxStudentCount){
         super(department, code, description, minStudentCount, maxStudentCount);
@@ -25,4 +31,12 @@ public class Session extends Course {
     public void makeID() {
         id = "c" + generatorAlgorithm.generateID();
     }
+
+    public ArrayList<Student> getStudents() { return students; }
+
+    public void setStudents(ArrayList<Student> students) { this.students = students; }
+
+    public Faculty getInstructor() { return instructor; }
+
+    public void setInstructor(Faculty instructor) { this.instructor = instructor; }
 }
